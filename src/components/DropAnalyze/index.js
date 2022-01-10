d48f4ce954ac5dbaeef03ed6539aec1f501350c5
skadandy <jy03179163@gmail.com>
2022-01-10 22:28:36 +0800

    新增物品图片。页面样式修改。

--- src/components/DropAnalyze/index.js
+++ src/components/DropAnalyze/index.js
@@ -1,194 +1,200 @@
-import React, { useState, useEffect } from "react";
-import {
-  Grid,
-  Divider,
-  Card,
-  CardMedia,
-  CardContent,
-  TextField,
-  CircularProgress,
-  CardHeader,
-  Typography
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { green, blue } from "@material-ui/core/colors";
+import React, {useEffect, useState} from "react";
+import {Box, Card, CardContent, CardMedia, Grid, IconButton, TextField} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete'
+import {makeStyles} from "@material-ui/core/styles";
 import ReactECharts from 'echarts-for-react';
 
-import ProbaChart from "./ProbaChart"
+import { getVirtulData } from './utils';
 
+import coronationRingImg from '../../assets/item/coronationRing.jpg';
+import intricacyRingImg from '../../assets/item/intricacyRing.jpg';
+import lineageRingImg from '../../assets/item/lineageRing.jpg';
+import goldBrickImg from '../../assets/item/goldBrick.jpg';
 
-import crystalImg from "../../assets/draws/crystal.jpg";
-import singleTicketImg from "../../assets/draws/signle-roll-ticket.jpg";
-import tenRollTicketImg from "../../assets/draws/10-roll-ticket.jpg";
+const PROTO_BAHAMUT = 0; // 大巴
+const AKASHA = 1; // akx
+const GRAND_ORDER = 2; // 大公
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    padding: 10
-  },
-  card: {
-    display: "flex",
-    padding: 10,
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        padding: 10
+    },
+    card: {
+        display: "flex",
+        padding: 10,
 
-    margin: "auto",
-    [theme.breakpoints.up("sm")]: {
-      flex: 1,
-      margin: 5
+        margin: "auto",
+        [theme.breakpoints.up("sm")]: {
+            flex: 1,
+            margin: 5
+        }
+    },
+    image: {
+        width: "auto",
+        height: "auto",
+        margin: "auto",
+        maxWidth: "25%",
+        maxHeight: 86.776
     }
-  },
-  image: {
-    width: "auto",
-    height: "auto",
-    margin: "auto",
-    maxWidth: "25%",
-    maxHeight: 86.776
-  }
 }));
 
 export default function DropAnalyze(props) {
-  const [crystal, setCrystal] = useState(0);
-  const [singleRollTicket, setSingleRollTicket] = useState(0);
-  const [tenRollTicket, setTenRollTicket] = useState(0);
+    const [coronationRing, setCoronationRing] = useState(0);
+    const [lineageRing, setLineageRing] = useState(0);
+    const [intricacyRing, setIntricacyRing] = useState(0);
+    const [goldBrick, setGoldBrick] = useState(0);
 
-  useEffect(() => {
-    let state = JSON.parse(localStorage.getItem("SparkCalculator"));
-    if (state !== null) {
-      setCrystal(state.crystal);
-      setSingleRollTicket(state.singleRollTicket);
-      setTenRollTicket(state.tenRollTicket);
+    useEffect(() => {
+        // todo 分类记录
+        let state = JSON.parse(localStorage.getItem("DropAnalyze"));
+        if (state !== null) {
+            setCoronationRing(state.coronationRing);
+            setLineageRing(state.lineageRing);
+            setIntricacyRing(state.intricacyRing);
+            setGoldBrick(state.goldBrick);
+        }
+    }, []);
+
+    useEffect(() => {
+        let state = {
+            coronationRing: coronationRing, // 普通戒指
+            lineageRing: lineageRing, // 黑戒指
+            intricacyRing: intricacyRing, // 红戒指
+            goldBrick: goldBrick // 金
+        };
+        localStorage.setItem("DropAnalyze", JSON.stringify(state));
+    }, [coronationRing, lineageRing, intricacyRing, goldBrick]);
+
+    const classes = useStyles();
+
+    const options = {
+        title: {
+            top: 30,
+            left: 'center',
+            text: 'Daily Step Count'
+        },
+        tooltip: {},
+        visualMap: {
+            min: 0,
+            max: 20,
+            type: 'piecewise',
+            orient: 'horizontal',
+            left: 'center',
+            top: 65
+        },
+        calendar: {
+            top: 120,
+            left: 30,
+            right: 30,
+            cellSize: ['auto', 13],
+            range: '2016',
+            itemStyle: {
+                borderWidth: 0.5
+            },
+            yearLabel: { show: false }
+        },
+        series: {
+            type: 'heatmap',
+            coordinateSystem: 'calendar',
+            data: getVirtulData('2022')
+        }
     }
-  }, []);
 
-  useEffect(() => {
-    let state = {
-      crystal: crystal,
-      singleRollTicket: singleRollTicket,
-      tenRollTicket: tenRollTicket
-    };
-    localStorage.setItem("SparkCalculator", JSON.stringify(state));
-  }, [crystal, singleRollTicket, tenRollTicket]);
-
-  const classes = useStyles();
-
-  let rolls = Math.floor(
-    (crystal + 300 * singleRollTicket + 3000 * tenRollTicket) / 300
-  );
-  let progress = rolls / 300;
-
-  const options = {
-    grid: {top: 8, right: 8, bottom: 24, left: 36},
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        smooth: true,
-      },
-    ],
-    tooltip: {
-      trigger: 'axis',
-    },
-  }
-
-  return (
-    <Grid container spacing={1}>
-      <Grid item xs={12}>
-        12121
-        <ReactECharts option={options}/>
-      </Grid>
-      <Grid item xs={12} className={classes.root}>
-        <Card className={classes.card}>
-          <CardMedia
-            component="img"
-            image={crystalImg}
-            className={classes.image}
-          />
-          <CardContent>
-            <TextField
-              name="crystal"
-              label="宝晶石"
-              variant="standard"
-              value={crystal}
-              onChange={e => {
-                setCrystal(Number(e.target.value));
-              }}
-            />
-          </CardContent>
-        </Card>
-        <Card className={classes.card}>
-          <CardMedia
-            component="img"
-            image={singleTicketImg}
-            className={classes.image}
-          />
-          <CardContent>
-            <TextField
-              name="crystal"
-              label="单抽券"
-              variant="standard"
-              value={singleRollTicket}
-              onChange={e => {
-                setSingleRollTicket(Number(e.target.value));
-              }}
-            />
-          </CardContent>
-        </Card>
-        <Card className={classes.card}>
-          <CardMedia
-            component="img"
-            image={tenRollTicketImg}
-            className={classes.image}
-          />
-          <CardContent>
-            <TextField
-              name="crystal"
-              label="10连券"
-              variant="standard"
-              value={tenRollTicket}
-              onChange={e => {
-                setTenRollTicket(Number(e.target.value));
-              }}
-            />
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader
-            title="掉落统计"
-            titleTypographyProps={{ variant: "h6" }}
-          />
-          <Divider />
-          <div style={{ padding: 10, display: "flex" }}>
-            <CircularProgress
-              style={
-                progress * 100 >= 100
-                  ? { color: green[500] }
-                  : { color: blue[700] }
-              }
-              variant="static"
-              value={progress * 100 > 100 ? 100 : progress * 100}
-              size={70}
-            />
-            <Typography variant="h6" style={{ margin: "auto" }}>
-              {(progress * 100).toFixed(2) + "%"}
-            </Typography>
-            <Typography variant="h6" style={{ margin: "auto" }}>
-              {rolls} / 300抽
-            </Typography>
-          </div>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <ProbaChart />
-      </Grid>
-    </Grid>
-  );
+    return (
+        <Grid container spacing={1}>
+            <Grid item xs={12} className={classes.root}>
+                <Card className={classes.card}>
+                    <CardMedia
+                        component="img"
+                        image={coronationRingImg}
+                        className={classes.image}
+                    />
+                    <CardContent>
+                        <Grid container spacing={1} direction="row">
+                            <Grid item sx={4}>
+                                <IconButton aria-label="delete">
+                                    <DeleteIcon />
+                                </IconButton>
+                            </Grid>
+                            <Grid item sx={4}>
+                                <TextField
+                                    name="coronationRing"
+                                    helperText="普通戒指"
+                                    value={coronationRing}
+                                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                                    onChange={e => {
+                                        setCoronationRing(Number(e.target.value));
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item sx={4}>
+                                <IconButton aria-label="delete">
+                                    <DeleteIcon />
+                                </IconButton>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardMedia
+                        component="img"
+                        image={lineageRingImg}
+                        className={classes.image}
+                    />
+                    <CardContent>
+                        <TextField
+                            name="lineageRing"
+                            label="黑戒指"
+                            variant="standard"
+                            value={lineageRing}
+                            onChange={e => {
+                                setLineageRing(Number(e.target.value));
+                            }}
+                        />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardMedia
+                        component="img"
+                        image={intricacyRingImg}
+                        className={classes.image}
+                    />
+                    <CardContent>
+                        <TextField
+                            name="intricacyRing"
+                            label="红戒指"
+                            variant="standard"
+                            value={intricacyRing}
+                            onChange={e => {
+                                setIntricacyRing(Number(e.target.value));
+                            }}
+                        />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardMedia
+                        component="img"
+                        image={goldBrickImg}
+                        className={classes.image}
+                    />
+                    <CardContent>
+                        <TextField
+                            name="goldBrick"
+                            label="FFJ"
+                            variant="standard"
+                            value={goldBrick}
+                            onChange={e => {
+                                setGoldBrick(Number(e.target.value));
+                            }}
+                        />
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12}>
+                <ReactECharts option={options}/>
+            </Grid>
+        </Grid>
+    );
 }
--- src/components/DropAnalyze/utils.js
+++ src/components/DropAnalyze/utils.js
@@ -1,38 +1,5 @@
-export const checkProbaFormat = value => {
-  if (isNaN(value) || Number(value) < 0 || Number(value) > 100)
-    return false;
-  return true;
-}
+import * as ReactECharts from 'echarts-for-react';
 
-export const checkRollFormat = value => {
-    if (isNaN(value) || !Number.isInteger(Number(value)) || Number(value) < 0)
-      return false;
-    return true;
-  }
-
-export const geoDistCDFReversed= (p, target) => {
-    return Math.log(1-target) / Math.log(1-p);
-}
-
-export const geoDistCDF = (p, k) => {
-    return 1 - (1 - p) ** k;
-}
-
-const factorial = n => {
-  if (n < 2) return 1;
-  let result = 1;
-  for (let i = 2; i <= n; i++) result *= i;
-  return result;
-}
-
-export const poissonDist = (k, p, n) => {
-  let L = n * p;
-  return (L ** k / factorial(k)) * Math.e ** (-L);
-}
-
-export const binomialDist = (k, p, n) => {
-  let coeff = 1;
-  for (let x = n-k+1; x <= n; x++) coeff *= x;
-  for (let x = 1; x <= k; x++) coeff /= x;
-  return coeff * (p ** k) * ((1 - p) ** (n - k))
+export const getVirtulData = year => {
+    return ['2022-01-01', '2023-01-01'];
 }
